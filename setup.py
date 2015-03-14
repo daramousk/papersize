978bee123e2a547bf4d02ec07f37bbc3809e1766
Louis <spalax@gresille.org>
2015-03-14 18:29:04 +0100

    Tests: Using tox

--- setup.py
+++ setup.py
@@ -53,7 +53,6 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.2",
-        "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
         "Topic :: Printing",
         "Topic :: Software Development :: Libraries :: Python Modules",
